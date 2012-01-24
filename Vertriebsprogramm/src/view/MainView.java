e1d7e02c868922be90467bd2eed9f5bb549caecb
Michael <haumic08@htlkaindorf.at>
2012-01-24 14:22:15 +0100

    Signed-off-by: Michael <haumic08@htlkaindorf.at>

--- Vertriebsprogramm/src/view/MainView.java
+++ Vertriebsprogramm/src/view/MainView.java
@@ -197,17 +197,17 @@ public class MainView extends javax.swing.JFrame {
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(251, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
                 .addComponent(jButton1)
-                .addGap(175, 175, 175))
+                .addContainerGap(416, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(115, 115, 115)
+                .addContainerGap()
                 .addComponent(jButton1)
-                .addContainerGap(228, Short.MAX_VALUE))
+                .addContainerGap(332, Short.MAX_VALUE))
         );
 
         jPanel3.add(jPanel2);
